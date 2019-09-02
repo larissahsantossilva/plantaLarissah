31a355d90042f732b95d5f475b7c62b925766147
larissahsantossilva <54816334+larissahsantossilva@users.noreply.github.com>
2019-09-02 20:17:55 -0300

    Add files via upload

---
+++ Ornamental.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planta_Larissah_1703323
+{
+	public class Ornamental : Planta
+	{
+		public string Paisagem { get; set; }
+		public string GerarHash()
+		{
+			return "";
+		}
+		public Ornamental()
+		{
+
+		}
+		public Ornamental(string _paisagem)
+		{
+			Paisagem = _paisagem;
+		}
+		public List<object> GerarLista()
+		{
+			List<object> listaPlantas = new List<object>();
+			listaPlantas.Add(new Ornamental("Lirio"));
+			return listaPlantas;
+		}
+
+	}
+}
